91a6106adbb3fab571fa13cf56d4efbb43df9919
Leela Sri <leelasric@gmail.com>
2025-01-24 20:02:13 +0530

    triangle

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,12 @@
+def sierpinski(n):
+    triangle = ["*"]
+    for i in range(n):
+        new_row = []
+        for row in triangle:
+            spaces = " " * len(row)
+            new_row.append(row + " " + row)
+            triangle = [row + spaces + row for row in triangle]
+        triangle += new_row
+    return "\n".join(triangle)
 
-print("Hello world")
+print(sierpinski(5))
